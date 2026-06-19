cd63b1e3632e31dc87523e07f800b02e15c38650
Alina_Mavlekaeva <alinamavlekaeva@gmail.com>
2026-06-19 15:17:45 +0300

    feat: initialize project

---
+++ cmd/gendiff/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:  "gendiff",
+		Usage: "Compares two configuration files and shows a difference.",
+	}
+	cmd.Run(context.Background(), os.Args)
+}
