d7c608fccb2f1645bccc77676351288b614aed04
Alina_Mavlekaeva <alinamavlekaeva@gmail.com>
2026-07-14 10:30:42 +0300

    fix: update gendiff_test.go

--- code/gendiff_test.go
+++ code/gendiff_test.go
@@ -1,6 +1,7 @@
 package code
 
 import (
+	"parsers"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,31 +10,32 @@ import (
 func TestGenDiff_JSON(t *testing.T) {
 	path1 := "../testdata/fixture/file1.json"
 	path2 := "../testdata/fixture/file2.json"
-	want := "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}"
-	got, err := genDiff(path1, path2)
-	if err != nil {
-		t.Fatalf("Unexpected genDiff error: %v", err)
+	want := AST{
+		Children: []AST{
+			{Key: "follow", Value: false, Status: "old"}, {Key: "host", Value: "hexlet.io", Status: "same"}, {Key: "proxy", Value: "123.234.53.22", Status: "old"}, {Key: "timeout", Value: float64(50), Status: "old"}, {Key: "timeout", Value: float64(20), Status: "new"}, {Key: "verbose", Value: true, Status: "new"},
+		},
 	}
+	info1, err := parsers.Parse(path1)
+	info2, err := parsers.Parse(path2)
+	if err != nil {
+		t.Fatalf("Unexpected Parse error: %v", err)
+	}
+	got := genDiff(info1, info2)
 	assert.Equal(t, want, got)
 }
 func TestGenDiff_YAML(t *testing.T) {
 	path1 := "../testdata/fixture/filepath1.yml"
 	path2 := "../testdata/fixture/filepath2.yml"
-	want := "{\n- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}"
-	got, err := genDiff(path1, path2)
-	if err != nil {
-		t.Fatalf("Unexpected genDiff error: %v", err)
+	want := AST{
+		Children: []AST{
+			{Key: "follow", Value: false, Status: "old"}, {Key: "host", Value: "hexlet.io", Status: "same"}, {Key: "proxy", Value: "123.234.53.22", Status: "old"}, {Key: "timeout", Value: 50, Status: "old"}, {Key: "timeout", Value: 20, Status: "new"}, {Key: "verbose", Value: true, Status: "new"},
+		},
 	}
+	info1, err := parsers.Parse(path1)
+	info2, err := parsers.Parse(path2)
+	if err != nil {
+		t.Fatalf("Unexpected Parse error: %v", err)
+	}
+	got := genDiff(info1, info2)
 	assert.Equal(t, want, got)
 }
-
-// func TestParse(t *testing.T) {
-// 	path1 := "../testdata/fixture/filepath1.json"
-// 	//path2 := "../testdata/fixture/filepath2.json"
-// 	want := "data"
-// 	got, err := parsers.Parse(path1)
-// 	if err != nil {
-// 		t.Fatalf("Unexpected Parse error: %v", err)
-// 	}
-// 	assert.Equal(t, want, got)
-// }
